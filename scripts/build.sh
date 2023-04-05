ed845000c522d6877fbed481310caaadd664979c
ndemasie <nathan.demasie@gmail.com>
2023-04-05 18:58:37 +0200

    fix: workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+get_last_image() { docker images --format "{{.ID}} {{.CreatedAt}}" | sort -rk 2 | awk 'NR==1{print $1}'; }
+
 # Build
 docker image build --file "./docker/build/Dockerfile" --force-rm .
 
-image_id=$(docker images --format "{{.ID}} {{.CreatedAt}}" | sort -rk 2 | awk 'NR==1{print $1}')
+image_id=$(get_last_image)
 container_id=$(docker container create $image_id)
 
 docker cp $container_id:/root/dist ./dist

00b6fcad8059368fc0be2f97fda0db61b96194eb
ndemasie <nathan.demasie@gmail.com>
2023-05-29 15:03:40 +0200

    fix: adjust script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-ARGS="$@"
-
 # DESCRIPTION
 # Bash script which builds the app in a docker container and copies out ./dist
 
 get_last_image() { docker images --format "{{.ID}} {{.CreatedAt}}" | sort --reverse --key 2 | awk 'NR==1{print $1}'; }
 
 # Build
-docker image build --force-rm "$ARGS" .
+docker image build $@ --force-rm .
 
 image_id=$(get_last_image)
 container_id=$(docker container create $image_id)

5dc292fa964390a9576a5faf5f380570f5d098ab
ndemasie <nathan.demasie@gmail.com>
2023-05-31 20:24:43 +0200

    bug: align https requests

--- site/src/apps/TutorialI18N/context/webSocket.tsx
+++ site/src/apps/TutorialI18N/context/webSocket.tsx
@@ -40,7 +40,7 @@ const Context = () => {
 
   const { sendMessage, lastMessage, readyState } = useLibWebSocket(
     import.meta.env?.PROD
-      ? 'ws://ec2-3-75-85-223.eu-central-1.compute.amazonaws.com/'
+      ? 'wss://ec2-3-75-85-223.eu-central-1.compute.amazonaws.com/'
       : 'ws://localhost:3001/',
     {
       onOpen: () => console.log('opened'),

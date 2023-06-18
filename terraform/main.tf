551e44609a79c7226180bc8b7967d0092c9575d0
ndemasie <nathan.demasie@gmail.com>
2023-06-18 16:01:24 +0200

    deprecate terraform use

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,108 +11,17 @@ locals {
   env = { for tuple in regexall("(.*)=\"(.*)\"", file("../.env")) : tuple[0] => tuple[1] }
 }
 
-variable "aws_variables" {
-  type = map(string)
-  default = {
-    ami           = "ami-04e601abe3e1a910f" # Ubuntu 22.04 LTS
-    instance_type = "t2.micro"              # Free tier
-    subnet        = "subnet-0428cd590bc209832"
-    vpc_id        = "vpc-0fbe0c741b97a6e17" # AWS Virtual Private Cloud
-    sec_group_id  = "sg-08361cdbc600b7f85"  # Security Group Id`
-    public_ip     = true
-    key_name      = "demasie_key_pair"
-  }
-}
-
 provider "aws" {
   region     = "eu-central-1"
   access_key = local.env["AWS_ACCESS_KEY_ID"]
   secret_key = local.env["AWS_SECRET_ACCESS_KEY"]
 }
 
-# Security Group
-resource "aws_security_group" "demaise_security_group" {
-  name        = "demasie_Security_Group"
-  description = "demasie_Security_Group"
-  vpc_id      = lookup(var.aws_variables, "vpc_id")
-
-  // Allow SSH
-  ingress {
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Allow HTTP
-  ingress {
-    from_port   = 80
-    protocol    = "tcp"
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Allow HTTPS
-  ingress {
-    from_port   = 443
-    protocol    = "tcp"
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# EC2 Server
-resource "aws_instance" "demasie_ec2_server" {
-  ami                         = lookup(var.aws_variables, "ami")
-  instance_type               = lookup(var.aws_variables, "instance_type")
-  subnet_id                   = lookup(var.aws_variables, "subnet")
-  associate_public_ip_address = lookup(var.aws_variables, "public_ip")
-  key_name                    = lookup(var.aws_variables, "key_name")
-
-  vpc_security_group_ids = [
-    aws_security_group.demaise_security_group.id
-  ]
-
-  user_data = <<EOF
-    #! /bin/sh
-    # Setup docker on server
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sudo sh ./get-docker.sh --dry-run
-  EOF
-
-  root_block_device {
-    delete_on_termination = true
-    iops                  = 3000
-    volume_size           = 8
-    volume_type           = "gp3"
-  }
-
-  depends_on = [aws_security_group.demaise_security_group]
-}
-
-output "ec2_instance" {
-  value = {
-    public_dns = aws_instance.demasie_ec2_server.public_dns,
-    public_ip  = aws_instance.demasie_ec2_server.public_ip
-  }
-}
-
 # Budget
-resource "aws_budgets_budget" "ec2_server" {
-  name         = "demasie EC2 Month Budget"
+resource "aws_budgets_budget" "demasie_month_budget" {
+  name         = "demasie-month-budget"
   budget_type  = "COST"
-  limit_amount = 5
+  limit_amount = 8
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 
@@ -140,3 +49,108 @@ resource "aws_budgets_budget" "ec2_server" {
     subscriber_email_addresses = split(",", local.env["AWS_BUDGET_SUBSCRIBERS"])
   }
 }
+
+# # VPC
+# resource "aws_vpc" "demasie_vpc" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
+
+#   tags = {
+#     Name = "demasie-vpc"
+#   }
+# }
+
+# # Subnet
+# resource "aws_subnet" "demasie_subnet" {
+#   vpc_id     = aws_vpc.demasie_vpc.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "demasie-submet"
+#   }
+# }
+
+# # Security Group
+# resource "aws_security_group" "demasie_security_group" {
+#   name        = "demasie-security-group"
+#   description = "demasie_security_group"
+#   vpc_id      = aws_vpc.demasie_vpc.id
+
+#   ingress {
+#     from_port   = 22
+#     protocol    = "tcp"
+#     to_port     = 22
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     from_port   = 80
+#     protocol    = "tcp"
+#     to_port     = 80
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     from_port   = 443
+#     protocol    = "tcp"
+#     to_port     = 443
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+# # EC2 Server
+# resource "aws_instance" "demasie_ec2_server" {
+#   ami                         = "ami-04e601abe3e1a910f" # Ubuntu 22.04 LTS
+#   instance_type               = "t2.nano"               # Free tier
+#   subnet_id                   = aws_subnet.demasie_subnet.id
+#   associate_public_ip_address = true
+#   key_name                    = "demasie_key_pair"
+#   count                       = 1
+
+#   tags = {
+#     Name = "demasie-ec2-server"
+#   }
+
+#   vpc_security_group_ids = [
+#     aws_security_group.demasie_security_group.id
+#   ]
+
+#   user_data = <<EOF
+#     #! /bin/sh
+
+# # Setup docker
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh ./get-docker.sh
+
+# # Add user to docker group
+# sudo usermod --append --groups docker $USER
+#   EOF
+
+#   root_block_device {
+#     delete_on_termination = true
+#     iops                  = 3000
+#     volume_size           = 8
+#     volume_type           = "gp3"
+#   }
+
+#   depends_on = [aws_security_group.demasie_security_group]
+# }
+
+# output "ec2_instance" {
+#   value = {
+#     public_dns = aws_instance.demasie_ec2_server.public_dns,
+#     public_ip  = aws_instance.demasie_ec2_server.public_ip
+#   }
+# }
+

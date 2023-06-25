617dfdac5c32f5374861a1d438189f37daf9e26c
ndemasie <nathan.demasie@gmail.com>
2023-06-25 18:28:27 +0200

    chore: update some schema and strings

--- apps/homepage/src/locales/index.ts
+++ apps/homepage/src/locales/index.ts
@@ -7,14 +7,13 @@ import deStrings from './de/strings.json'
 import enSchema from './en/schema.json'
 import enStrings from './en/strings.json'
 
-const parsedENSchema = await $RefParser.dereference(enSchema)
-const parsedDESchema = await $RefParser.dereference(deSchema)
-
 const strings = {
   en: _.merge({}, enStrings),
   de: _.merge({}, enStrings, deStrings),
 }
 
+const parsedENSchema = await $RefParser.dereference(enSchema)
+const parsedDESchema = await $RefParser.dereference(deSchema)
 const schema = {
   en: _.merge({}, parsedENSchema),
   de: _.merge({}, parsedENSchema, parsedDESchema),
@@ -24,10 +23,9 @@ function t(key: string | string[], lng: Language) {
   return (
     [key]
       .flat(Infinity)
-      .map(
-        (key) =>
-          _.get(strings?.[lng], key) || _.get(strings?.[Language.EN], key),
-      )
+      .map((key) => {
+        return _.get(strings?.[lng], key) || _.get(strings?.[Language.EN], key)
+      })
       .filter(Boolean)
       .at(0) || key
   )
--- apps/homepage/src/types/Page.ts
+++ apps/homepage/src/types/Page.ts
@@ -1,4 +1,5 @@
 export enum Page {
+  App = 'app',
   Blog = 'blog',
   CV = 'cv',
   Tutorial = 'tutorial',

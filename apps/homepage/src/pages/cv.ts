16458114fca283d150d3345686412e22367dc0b9
ndemasie <nathan.demasie@gmail.com>
2023-08-27 01:23:24 +0200

    fix: build path url false

--- apps/homepage/src/pages/cv.ts
+++ apps/homepage/src/pages/cv.ts
@@ -1,16 +1,18 @@
 import type { APIContext } from 'astro'
 
+import { readFileSync } from 'node:fs'
+import path from 'node:path'
+
 const CV_NAME = 'Nathan_DeMasie_CV'
 
 export async function get(ctx: APIContext) {
-  const url = new URL(`doc/${CV_NAME}.pdf`, ctx.site)
-  const response = await fetch(url)
-  const buffer = await response.arrayBuffer()
+  const file = path.join(process.cwd(), 'public', 'doc', `${CV_NAME}.pdf`)
+  const buffer = Buffer.from(readFileSync(file, 'binary'), 'binary')
 
   return new Response(buffer, {
     headers: {
       'Content-Type': 'application/pdf',
-      'Content-Disposition': `inline; filename="${CV_NAME}.pdf"`,
+      'Content-Disposition': `inline; filename="${CV_NAME}.pdf`,
     },
   })
 }
